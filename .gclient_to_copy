52ec65ecb3376964ac7fce46d0901b882f50874c
Artem Borovik <polzovatellllk@gmail.com>
2024-08-25 18:36:29 +0300

    initial

---
+++ .gclient_to_copy
@@ -0,0 +1,9 @@
+solutions = [
+  {
+    "name": ".",
+    "url": "https://chromium.googlesource.com/angle/angle.git",
+    "deps_file": "DEPS",
+    "managed": False,
+    "custom_vars": {},
+  },
+]
---
+++ build_macos.sh
@@ -0,0 +1,50 @@
+
+set -ex
+
+cd $(dirname $0)
+
+# clone depot_tools if not exists
+[ -d "depot_tools" ] || git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+
+export PATH="$(pwd)/depot_tools:$PATH"
+
+[ -d "angle" ] || mkdir angle
+[ -f "angle/.gclient" ] || cp .gclient_to_copy angle/.gclient
+
+
+[ -d "artifacts" ] || mkdir artifacts
+rm -rf artifacts/*
+
+cd angle
+gclient sync
+for TARGET_CPU in x64 arm64
+do
+    gn gen out/Mac/$TARGET_CPU "--args=\
+        is_debug=false \
+        is_component_build=false \
+        target_cpu=\"$TARGET_CPU\" \
+        angle_build_all=false \
+        angle_enable_null=false \
+        angle_has_frame_capture=false \
+        angle_enable_gl=false \
+        angle_enable_vulkan=false \
+        angle_enable_d3d9=false \
+        angle_enable_d3d11=false \
+        angle_enable_gl=false \
+        angle_enable_null=false \
+        angle_enable_metal=true \
+        angle_enable_essl=false \
+        angle_enable_wgpu=false \
+        angle_enable_glsl=true \
+    "
+    autoninja -C out/Mac/$TARGET_CPU libGLESv2 libEGL
+
+    mkdir ../artifacts/$TARGET_CPU
+    cp out/Mac/$TARGET_CPU/libEGL.dylib out/Mac/$TARGET_CPU/libGLESv2.dylib ../artifacts/$TARGET_CPU
+done
+cd ..
+mkdir artifacts/fat
+for LIB in libEGL libGLESv2
+do
+    lipo -create artifacts/x64/$LIB.dylib artifacts/arm64/$LIB.dylib -output artifacts/fat/$LIB.dylib
+done
